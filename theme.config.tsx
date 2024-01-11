a0ed188842b04636444b875d90ad45cebbdd0268
Samyog Dhital <dhitalsamyog123@gmail.com>
2024-01-11 11:56:24 +0545

    chore: update general details

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,12 +4,12 @@ import { DocsThemeConfig } from 'nextra-theme-docs';
 const config: DocsThemeConfig = {
   logo: <span>SpecuX</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/thespecux/documentation',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.gg/5vr4N6yn',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/thespecux/documentation',
   footer: {
     text: 'SpecuX - News Prediction Platform',
   },
