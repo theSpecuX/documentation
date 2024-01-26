2ba31c150f87595971bab7c49bcaee5acc39df12
Samyog Dhital <dhitalsamyog123@gmail.com>
2024-01-26 11:40:31 +0545

    chore: seo update

--- theme.config.tsx
+++ theme.config.tsx
@@ -13,6 +13,27 @@ const config: DocsThemeConfig = {
   footer: {
     text: 'SpecuX - Controversial News Prediction Platform',
   },
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s – SWR',
+      title: 'SpecuX - Documentation Guide',
+      description:
+        'SpecuX aim to highlight the value of prediction markets, especially when mainstream media conceals crucial information.',
+      openGraph: {
+        title: 'SpecuX - Documentation Guide',
+        description:
+          'SpecuX aim to highlight the value of prediction markets, especially when mainstream media conceals crucial information.',
+        // images : []
+      },
+    };
+  },
+  // head: (
+  //   <>
+  //     <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+  //     <meta property='og:title' content='Nextra' />
+  //     <meta property='og:description' content='The next site builder' />
+  //   </>
+  // ),
 };
 
 export default config;
